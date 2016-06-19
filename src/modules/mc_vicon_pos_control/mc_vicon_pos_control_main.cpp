b97602f74c47f1b4eadd013bf382c4af45785d5f
XIEHUI <hui.xie.sheffield@gmail.com>
2016-06-19 13:12:57 +0800

    nav2control_mode doc added

--- src/modules/mc_vicon_pos_control/mc_vicon_pos_control_main.cpp
+++ src/modules/mc_vicon_pos_control/mc_vicon_pos_control_main.cpp
@@ -11,23 +11,34 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <poll.h>
+
+#include <px4_posix.h>
+
+#include <platforms/px4_defines.h>
+
+
+#include <drivers/drv_hrt.h>
 #include <systemlib/scheduling_priorities.h>
 #include <systemlib/systemlib.h>
 #include <systemlib/param/param.h>
 #include <systemlib/err.h>
-#include <drivers/drv_hrt.h>
-#include <math.h>
-//#include <errno.h>
-//#include <termios.h>
-//#include "mc_vicon_pos_control.hpp"
+#include <systemlib/mavlink_log.h>
 
+#include <controllib/blocks.hpp>
+#include <controllib/block/BlockParam.hpp>
+#include <mathlib/mathlib.h>
+
+//#include <uORB/topics/vehicle_local_position.h>
+//#include <uORB/topics/control_state.h>
 
 /**
  * Deamon management function.
  */
 extern "C" __EXPORT int mc_vicon_pos_control_main(int argc, char *argv[]);
 
-class ViconControl
+class ViconControl:public control::SuperBlock
 {
 public:
     /* Constructor */
@@ -43,17 +54,77 @@ private:
 
     /* Subscription descriptors */
     int _local_pos_sub;
-//    int vicon_sub;
-//    int att_sub;
-//    int image_att_sp_sub;
-//    int control_mode_sub;
-//    int vehicle_status_sub;
-//    int param_sub;
-//    int rc_channels_sub;
-//    int manual_control_sp_sub;
-//    int predictor_sub;
-//    int R_bias_sub;
+    int _vehicle_status_sub;
+    int _params_sub;
+    int _arming_sub;
+    int _ctrl_state_sub;
+    int	_manual_sub;
+    int _control_mode_sub;
 
+    /* Subscribe structures */
+    struct vehicle_status_s _vehicle_status;
+    struct vehicle_local_position_s _local_pos;
+    struct actuator_armed_s _arming;
+    struct control_state_s _ctrl_state;
+    struct manual_control_setpoint_s _manual;
+    struct vehicle_control_mode_s _control_mode;
+
+    void poll_subscriptions();
+
+    struct{
+        param_t thr_min;
+        param_t thr_max;
+        param_t thr_hover;
+
+        param_t z_p;
+        param_t z_vel_p;
+        param_t z_vel_i;
+        param_t z_vel_d;
+        param_t z_vel_max_up;
+        param_t z_vel_max_down;
+        param_t z_ff;
+
+        param_t xy_p;
+        param_t xy_vel_p;
+        param_t xy_vel_i;
+        param_t xy_vel_d;
+        param_t xy_vel_max;
+        param_t xy_ff;
+    } _params_handles;
+
+    struct{
+        float thr_min;
+        float thr_max;
+        float thr_hover;
+        float vel_max_up;
+        float vel_max_down;
+
+        math::Vector<3> pos_p;
+        math::Vector<3> vel_p;
+        math::Vector<3> vel_i;
+        math::Vector<3> vel_d;
+        math::Vector<3> vel_ff;
+        math::Vector<3> vel_max;
+    } _params;
+
+    int parameters_update(bool force);
+
+    /* publication topics */
+    orb_advert_t _mavlink_log_pub;
+
+
+    control::BlockDerivative _vel_x_deriv;
+    control::BlockDerivative _vel_y_deriv;
+    control::BlockDerivative _vel_z_deriv;
+
+    math::Matrix<3, 3> _R;
+    math::Vector<3> _pos;
+    math::Vector<3> _vel;
+    math::Vector<3> _vel_sp;
+    math::Vector<3> _vel_ff;
+    math::Vector<3> _vel_err_d;
+
+    float _yaw;
 
     void task_main();
 };
@@ -63,15 +134,79 @@ namespace Vicon_Control
     ViconControl *g_control;
 }
 
-ViconControl::ViconControl(){
+ViconControl::ViconControl():
+    SuperBlock(NULL, "VCN"),
+    _task_should_exit(false),
+    _control_task(-1),
+
+    /* subscriptions */
+    _local_pos_sub(-1),
+    _vehicle_status_sub(-1),
+    _params_sub(-1),
+    _arming_sub(-1),
+    _ctrl_state_sub(-1),
+    _manual_sub(-1),
+    _control_mode_sub(-1),
+    /*subscribed structure*/
+    _vehicle_status{},
+    _local_pos{},
+    _arming{},
+    _ctrl_state{},
+    _manual{},
+    _control_mode{},
+    /*publication*/
+    _mavlink_log_pub(nullptr),
+
+    _vel_x_deriv(this, "VELD"),
+    _vel_y_deriv(this, "VELD"),
+    _vel_z_deriv(this, "VELD"),
+
+    _yaw(0.0f)
+{
+    // Make the quaternion valid for control state
+    _ctrl_state.q[0] = 1.0f;
+
+
+    _params.pos_p.zero();
+    _params.vel_p.zero();
+    _params.vel_i.zero();
+    _params.vel_d.zero();
+    _params.vel_max.zero();
+    _params.vel_ff.zero();
+
+    _R.identity();
+
+    _pos.zero();
+//    _pos_sp.zero();
+    _vel.zero();
+    _vel_sp.zero();
+    _vel_ff.zero();
+
+
+    _params_handles.thr_min		= param_find("MPC_THR_MIN");
+    _params_handles.thr_max		= param_find("MPC_THR_MAX");
+    _params_handles.thr_hover	= param_find("MPC_THR_HOVER");
+
+    _params_handles.z_p		= param_find("MPC_Z_P");
+    _params_handles.z_vel_p		= param_find("MPC_Z_VEL_P");
+    _params_handles.z_vel_i		= param_find("MPC_Z_VEL_I");
+    _params_handles.z_vel_d		= param_find("MPC_Z_VEL_D");
+    _params_handles.z_vel_max_up	= param_find("MPC_Z_VEL_MAX_UP");
+    _params_handles.z_vel_max_down	= param_find("MPC_Z_VEL_MAX");
+    _params_handles.z_ff		= param_find("MPC_Z_FF");
+
+    _params_handles.xy_p		= param_find("MPC_XY_P");
+    _params_handles.xy_vel_p	= param_find("MPC_XY_VEL_P");
+    _params_handles.xy_vel_i	= param_find("MPC_XY_VEL_I");
+    _params_handles.xy_vel_d	= param_find("MPC_XY_VEL_D");
+    _params_handles.xy_vel_max	= param_find("MPC_XY_VEL_MAX");
+    _params_handles.xy_ff		= param_find("MPC_XY_FF");
+
+    /* fetch initial parameter values */
+    parameters_update(true);
 
 }
 
-void ViconControl::task_main(){
-    /*Do subscription */
-    _local_pos_sub = orb_subscribe(ORB_ID(vehicle_local_position));
-
-}
 
 ViconControl::~ViconControl(){
     if (_control_task != -1) {
@@ -95,6 +230,200 @@ ViconControl::~ViconControl(){
     Vicon_Control::g_control = nullptr;
 }
 
+
+void ViconControl::poll_subscriptions()
+{
+    bool updated;
+
+    orb_check(_vehicle_status_sub, &updated);
+    if (updated) {
+        orb_copy(ORB_ID(vehicle_status), _vehicle_status_sub, &_vehicle_status);
+    }
+
+    orb_check(_local_pos_sub,&updated);
+    if (updated) {
+        orb_copy(ORB_ID(vehicle_local_position), _local_pos_sub, &_local_pos);
+    }
+
+    orb_check(_arming_sub,&updated);
+    if(updated){
+        orb_copy(ORB_ID(actuator_armed),_arming_sub,&_arming);
+    }
+
+    orb_check(_ctrl_state_sub, &updated);
+    if (updated) {
+        orb_copy(ORB_ID(control_state), _ctrl_state_sub, &_ctrl_state);
+
+        /* get current rotation matrix and euler angles from control state quaternions */
+        math::Quaternion q_att(_ctrl_state.q[0], _ctrl_state.q[1], _ctrl_state.q[2], _ctrl_state.q[3]);
+        _R = q_att.to_dcm();
+        math::Vector<3> euler_angles;
+        euler_angles = _R.to_euler();
+        _yaw = euler_angles(2);
+    }
+
+    orb_check(_manual_sub, &updated);
+    if (updated) {
+        orb_copy(ORB_ID(manual_control_setpoint), _manual_sub, &_manual);
+    }
+
+    orb_check(_control_mode_sub, &updated);
+    if (updated) {
+        orb_copy(ORB_ID(vehicle_control_mode), _control_mode_sub, &_control_mode);
+    }
+
+
+}
+
+int ViconControl::parameters_update(bool force)
+{
+    bool updated;
+    struct parameter_update_s param_upd;
+
+    orb_check(_params_sub, &updated);
+
+    if (updated) {
+        orb_copy(ORB_ID(parameter_update), _params_sub, &param_upd);
+    }
+
+    if (updated || force) {
+
+        param_get(_params_handles.thr_min, &_params.thr_min);
+        param_get(_params_handles.thr_max, &_params.thr_max);
+        param_get(_params_handles.thr_hover, &_params.thr_hover);
+
+        float v;
+        param_get(_params_handles.xy_p, &v);
+        _params.pos_p(0) = v;
+        _params.pos_p(1) = v;
+        param_get(_params_handles.z_p, &v);
+        _params.pos_p(2) = v;
+        param_get(_params_handles.xy_vel_p, &v);
+        _params.vel_p(0) = v;
+        _params.vel_p(1) = v;
+        param_get(_params_handles.z_vel_p, &v);
+        _params.vel_p(2) = v;
+        param_get(_params_handles.xy_vel_i, &v);
+        _params.vel_i(0) = v;
+        _params.vel_i(1) = v;
+        param_get(_params_handles.z_vel_i, &v);
+        _params.vel_i(2) = v;
+        param_get(_params_handles.xy_vel_d, &v);
+        _params.vel_d(0) = v;
+        _params.vel_d(1) = v;
+        param_get(_params_handles.z_vel_d, &v);
+        _params.vel_d(2) = v;
+        param_get(_params_handles.xy_vel_max, &v);
+        _params.vel_max(0) = v;
+        _params.vel_max(1) = v;
+        param_get(_params_handles.z_vel_max_up, &v);
+        _params.vel_max_up = v;
+        _params.vel_max(2) = v;
+        param_get(_params_handles.z_vel_max_down, &v);
+        _params.vel_max_down = v;
+
+        param_get(_params_handles.xy_ff, &v);
+        v = math::constrain(v, 0.0f, 1.0f);
+        _params.vel_ff(0) = v;
+        _params.vel_ff(1) = v;
+        param_get(_params_handles.z_ff, &v);
+        v = math::constrain(v, 0.0f, 1.0f);
+        _params.vel_ff(2) = v;
+    }
+
+    return OK;
+}
+
+void ViconControl::task_main(){
+
+    /*Do subscription */
+    _local_pos_sub = orb_subscribe(ORB_ID(vehicle_local_position));
+    _vehicle_status_sub = orb_subscribe(ORB_ID(vehicle_status));
+    _params_sub = orb_subscribe(ORB_ID(parameter_update));
+    _arming_sub = orb_subscribe(ORB_ID(actuator_armed));
+    _manual_sub = orb_subscribe(ORB_ID(manual_control_setpoint));
+    _control_mode_sub = orb_subscribe(ORB_ID(vehicle_control_mode));
+
+
+    /* initialize values of critical structs until first regular update */
+    _arming.armed = false;
+    bool was_armed = false;
+
+    poll_subscriptions();
+
+
+    hrt_abstime t_prev = 0;
+
+    px4_pollfd_struct_t fds[1];
+    fds[0].fd = _local_pos_sub;
+    fds[0].events = POLLIN;
+
+    while (!_task_should_exit){
+        /* wait for up to 500ms for data */
+        int pret = px4_poll(&fds[0], (sizeof(fds) / sizeof(fds[0])), 500);
+
+        /* timed out - periodic check for _task_should_exit */
+        if (pret == 0) {
+            continue;
+        }
+        /* this is undesirable but not much we can do */
+        if (pret < 0) {
+            warn("poll error %d, %d", pret, errno);
+            continue;
+        }
+
+        poll_subscriptions();
+        parameters_update(false);
+
+        hrt_abstime t = hrt_absolute_time();
+        float dt = t_prev != 0 ? (t - t_prev) * 0.000001f : 0.0f;
+        t_prev = t;
+        setDt(dt);
+
+        if (_control_mode.flag_armed && !was_armed) {
+
+        }
+        //Update previous arming state
+        was_armed = _control_mode.flag_armed;
+
+
+        /* Update velocity derivative,
+         * independent of the current flight mode
+         */
+        if (_local_pos.timestamp > 0) {
+
+            if (PX4_ISFINITE(_local_pos.x) &&
+                    PX4_ISFINITE(_local_pos.y) &&
+                    PX4_ISFINITE(_local_pos.z)) {
+                _pos(0) = _local_pos.x;
+                _pos(1) = _local_pos.y;
+                _pos(2) = _local_pos.z;
+            }
+
+            if (PX4_ISFINITE(_local_pos.vx) &&
+                    PX4_ISFINITE(_local_pos.vy) &&
+                    PX4_ISFINITE(_local_pos.vz)) {
+                _vel(0) = _local_pos.vx;
+                _vel(1) = _local_pos.vy;
+                _vel(2) = _local_pos.vz;
+            }
+
+            _vel_err_d(0) = _vel_x_deriv.update(-_vel(0));
+            _vel_err_d(1) = _vel_y_deriv.update(-_vel(1));
+            _vel_err_d(2) = _vel_z_deriv.update(-_vel(2));
+        }
+        //TODO to check whether this will send log info on QGC
+//        mavlink_and_console_log_info(&_mavlink_log_pub, "[vcn] stopped");
+    }
+
+}
+
+
+void ViconControl::task_main_trampoline(int argc, char *argv[])
+{
+    Vicon_Control::g_control->task_main();
+}
+
 int ViconControl::start(){
     ASSERT(_control_task == -1);
 
@@ -112,12 +441,6 @@ int ViconControl::start(){
     }
 
     return OK;
-}
-
-void
-ViconControl::task_main_trampoline(int argc, char *argv[])
-{
-    Vicon_Control::g_control->task_main();
 }
 
 int mc_vicon_pos_control_main(int argc, char *argv[])
@@ -176,5 +499,6 @@ int mc_vicon_pos_control_main(int argc, char *argv[])
     warnx("unrecognized command");
     return 1;
 }
+
 
 
--- src/modules/mc_vicon_pos_control/mc_vicon_pos_control_params.c
+++ src/modules/mc_vicon_pos_control/mc_vicon_pos_control_params.c
@@ -1,34 +1,223 @@
-#include <systemlib/param/param.h>
-
-PARAM_DEFINE_FLOAT(VCN_X_KI,0);
-PARAM_DEFINE_FLOAT(VCN_X_KP,0);
-PARAM_DEFINE_FLOAT(VCN_X_KD,0);
-PARAM_DEFINE_FLOAT(VCN_X_MAX,0);
-PARAM_DEFINE_FLOAT(VCN_X_ISAT,0);
-
-PARAM_DEFINE_FLOAT(VCN_Y_KI,0);
-PARAM_DEFINE_FLOAT(VCN_Y_KP,0);
-PARAM_DEFINE_FLOAT(VCN_Y_KD,0);
-PARAM_DEFINE_FLOAT(VCN_Y_MAX,0);
-PARAM_DEFINE_FLOAT(VCN_Y_ISAT,0);
-
-PARAM_DEFINE_FLOAT(VCN_Z_KI,0);
-PARAM_DEFINE_FLOAT(VCN_Z_KP,0);
-PARAM_DEFINE_FLOAT(VCN_Z_KD,0);
-PARAM_DEFINE_FLOAT(VCN_Z_MAX,0);
-PARAM_DEFINE_FLOAT(VCN_Z_ISAT,0);
-
-PARAM_DEFINE_FLOAT(VCN_ROLL_MAX,0);
-PARAM_DEFINE_FLOAT(VCN_PITCH_MAX,0);
-PARAM_DEFINE_FLOAT(VCN_THRUST_MAX,0);
-PARAM_DEFINE_FLOAT(VCN_THRUST_MIN,0);
-PARAM_DEFINE_FLOAT(VCN_THRUST_G,0);
-
-PARAM_DEFINE_FLOAT(VXX_W,0);
-PARAM_DEFINE_FLOAT(VXX_R,0);
-
-PARAM_DEFINE_INT32(VCN_USE_DELAY,0);
-PARAM_DEFINE_INT32(VCN_USE_RBIAS,0);
 
 
+/**
+ * Minimum thrust in auto thrust control
+ *
+ * It's recommended to set it > 0 to avoid free fall with zero thrust.
+ *
+ * @unit norm
+ * @min 0.05
+ * @max 1.0
+ * @decimal 2
+ * @increment 0.01
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_THR_MIN, 0.12f);
+
+/**
+ * Hover thrust
+ *
+ * Vertical thrust required to hover.
+ * This value is mapped to center stick for manual throttle control.
+ * With this value set to the thrust required to hover, transition
+ * from manual to ALTCTL mode while hovering will occur with the
+ * throttle stick near center, which is then interpreted as (near)
+ * zero demand for vertical speed.
+ *
+ * @unit norm
+ * @min 0.2
+ * @max 0.8
+ * @decimal 2
+ * @increment 0.01
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_THR_HOVER, 0.5f);
+
+/**
+ * Maximum thrust in auto thrust control
+ *
+ * Limit max allowed thrust. Setting a value of one can put
+ * the system into actuator saturation as no spread between
+ * the motors is possible any more. A value of 0.8 - 0.9
+ * is recommended.
+ *
+ * @unit norm
+ * @min 0.0
+ * @max 0.95
+ * @decimal 2
+ * @increment 0.01
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_THR_MAX, 0.9f);
+
+/**
+ * Proportional gain for vertical position error
+ *
+ * @min 0.0
+ * @max 1.5
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_Z_P, 1.0f);
+
+/**
+ * Proportional gain for vertical velocity error
+ *
+ * @min 0.1
+ * @max 0.4
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_Z_VEL_P, 0.2f);
+
+/**
+ * Integral gain for vertical velocity error
+ *
+ * Non zero value allows hovering thrust estimation on stabilized or autonomous takeoff.
+ *
+ * @min 0.01
+ * @max 0.1
+ * @decimal 3
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_Z_VEL_I, 0.02f);
+
+/**
+ * Differential gain for vertical velocity error
+ *
+ * @min 0.0
+ * @max 0.1
+ * @decimal 3
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_Z_VEL_D, 0.0f);
+
+/**
+ * Maximum vertical ascent velocity
+ *
+ * Maximum vertical velocity in AUTO mode and endpoint for stabilized modes (ALTCTRL, POSCTRL).
+ *
+ * @unit m/s
+ * @min 0.5
+ * @max 8.0
+ * @decimal 1
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_Z_VEL_MAX_UP, 0.5f);
+
+/**
+ * Maximum vertical descent velocity
+ *
+ * Maximum vertical velocity in AUTO mode and endpoint for stabilized modes (ALTCTRL, POSCTRL).
+ *
+ * @unit m/s
+ * @min 0.5
+ * @max 4.0
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_Z_VEL_MAX, 1.0f);
+
+/**
+ * Transitional support, do not change / use
+ *
+ * @unit m/s
+ * @min 0.5
+ * @max 4.0
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_Z_VEL_MAX_DN, 1.0f);
+
+/**
+ * Vertical velocity feed forward
+ *
+ * Feed forward weight for altitude control in stabilized modes (ALTCTRL, POSCTRL). 0 will give slow responce and no overshot, 1 - fast responce and big overshot.
+ *
+ * @min 0.0
+ * @max 1.0
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_Z_FF, 0.5f);
+
+
+/**
+ * Proportional gain for horizontal position error
+ *
+ * @min 0.0
+ * @max 2.0
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_XY_P, 1.25f);
+
+/**
+ * Proportional gain for horizontal velocity error
+ *
+ * @min 0.06
+ * @max 0.15
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_XY_VEL_P, 0.09f);
+
+/**
+ * Integral gain for horizontal velocity error
+ *
+ * Non-zero value allows to resist wind.
+ *
+ * @min 0.0
+ * @max 0.1
+ * @decimal 3
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_XY_VEL_I, 0.02f);
+
+/**
+ * Differential gain for horizontal velocity error. Small values help reduce fast oscillations. If value is too big oscillations will appear again.
+ *
+ * @min 0.005
+ * @max 0.1
+ * @decimal 3
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_XY_VEL_D, 0.01f);
+
+
+/**
+ * Maximum horizontal velocity
+ *
+ * Maximum horizontal velocity in AUTO mode. If higher speeds
+ * are commanded in a mission they will be capped to this velocity.
+ *
+ * @unit m/s
+ * @min 0.0
+ * @max 20.0
+ * @increment 1
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_XY_VEL_MAX, 1.0f);
+
+/**
+ * Horizontal velocity feed forward
+ *
+ * Feed forward weight for position control in position control mode (POSCTRL). 0 will give slow responce and no overshot, 1 - fast responce and big overshot.
+ *
+ * @min 0.0
+ * @max 1.0
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_XY_FF, 0.5f);
+
+/**
+ * Low pass filter cut freq. for numerical velocity derivative
+ *
+ * @unit Hz
+ * @min 0.0
+ * @max 10
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(VCN_VELD_LP, 5.0f);
 
